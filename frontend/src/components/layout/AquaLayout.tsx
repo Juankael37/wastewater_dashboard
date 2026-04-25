adb44e94b5bdaa6aa6e39dca75ed6a20941bc243
Juankael37 <juankael37@gmail.com>
2026-04-25 17:49:50 +0800

    fix: clear caches for login

--- frontend/src/components/layout/AquaLayout.tsx
+++ frontend/src/components/layout/AquaLayout.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navigation from './Navigation'
-import Header from './Header'
+import AquaNavigation from './AquaNavigation'
+import AquaHeader from './AquaHeader'
 
 const AquaLayout: React.FC = () => {
   return (
     <div className="min-h-screen bg-slate-900">
-      <Header />
+      <AquaHeader />
       <div className="flex">
-        <Navigation />
+        <AquaNavigation />
         <main className="flex-1 p-6">
           <Outlet />
         </main>
